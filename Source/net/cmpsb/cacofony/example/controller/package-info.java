bef4f867618f0355ff235e8d0081b5503a234787
Luc Everse <luc@cmpsb.net>
2017-04-16 08:50:22 +0200

    Prepare for release.

--- Source/net/cmpsb/cacofony/example/controller/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Controllers for the example server.
- *
- * @author Luc Everse
- */
-package net.cmpsb.cacofony.example.controller;
--- Source/net/cmpsb/cacofony/example/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * An example server.
- *
- * @author Luc Everse
- */
-package net.cmpsb.cacofony.example;
