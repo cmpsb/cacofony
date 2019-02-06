e0f021055dc5b4c8e94b539234535f291b61d9c0
Luc Everse <luc@wukl.net>
2019-02-06 22:57:57 +0100

    Also test the SettingsFrame's getLength

--- src/test/java/net/wukl/cacofony/http2/frame/SettingsFrameTest.java
+++ src/test/java/net/wukl/cacofony/http2/frame/SettingsFrameTest.java
@@ -41,6 +41,7 @@ public class SettingsFrameTest {
         final var frame = new SettingsFrame(List.of(), false);
         final var bytes = new ByteArrayOutputStream();
         frame.writePayload(bytes);
+        assertThat(frame.getLength()).isZero();
         assertThat(bytes.size()).isZero();
     }
 
@@ -49,6 +50,7 @@ public class SettingsFrameTest {
         final var frame = SettingsFrame.ACK;
         final var bytes = new ByteArrayOutputStream();
         frame.writePayload(bytes);
+        assertThat(frame.getLength()).isZero();
         assertThat(bytes.size()).isZero();
     }
 
@@ -68,6 +70,7 @@ public class SettingsFrameTest {
         final var bytes = new ByteArrayOutputStream();
         frame.writePayload(bytes);
 
+        assertThat(frame.getLength()).isEqualTo(6);
         assertThat(bytes.toByteArray()).containsExactly(
                 0x00, 0x02,
                 0x00, 0x00, 0x00, 0x01
@@ -84,6 +87,7 @@ public class SettingsFrameTest {
         final var bytes = new ByteArrayOutputStream();
         frame.writePayload(bytes);
 
+        assertThat(frame.getLength()).isEqualTo(12);
         assertThat(bytes.toByteArray()).containsExactly(
             0x00, 0x01,
             0xf0, 0x0f, 0xf0, 0x0f,
